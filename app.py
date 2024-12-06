c4d56ece1373bb145ad90680471a37a843c11fd4
sozari <jpsouza.1709@gmail.com>
2024-12-06 17:05:56 -0300

    att

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, request, redirect, url_for
+from flask import Flask, jsonify, render_template, request, redirect, url_for, session
 import mysql.connector
 
 from aluno_update import aluno_update_bp
@@ -13,6 +13,7 @@ from adm_update import adm_update_bp
 from professor_update import professor_update_bp
 from professor_delete import professor_delete_bp
 from usuario_list import usuario_list_bp
+from login import login_bp
 
 
 
@@ -32,7 +33,9 @@ app.register_blueprint(adm_update_bp)
 app.register_blueprint(professor_update_bp)
 app.register_blueprint(professor_delete_bp)
 app.register_blueprint(usuario_list_bp)
+app.register_blueprint(login_bp)
 
+app.secret_key = 'minha_chave_secreta_123'  # Aqui estamos definindo uma chave secreta simples
 
 
 '''
@@ -40,10 +43,19 @@ app.register_blueprint(usuario_list_bp)
 def inicial():
     return render_template('usuario_list.html')'''
 
-
+'''
 @app.route('/', methods=['GET'])
 def inicial():
-    return render_template('inicial.html')
+    return render_template('login.html')'''
+    
+    
+# Rota inicial
+@app.route('/', methods=['GET'])
+def inicial():
+    # Verifica se o usuário está logado
+    if 'usuario_nome' in session:
+        return render_template('inicial.html', nome=session['usuario_nome'])  # Passando o nome para o template
+    return redirect(url_for('login_bp.login'))  # Se não estiver logado, redireciona para o login
 
 
 if __name__ == '__main__':
